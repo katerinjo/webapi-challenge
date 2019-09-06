03ed7eece5ddbe63da5d7d68d6b2fc65a364734c
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-06 11:25:53 -0700

    can create actions

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const db = require('../data/helpers/projectModel');
+const db = require('../data/helpers/actionModel');
 
 const router = express.Router();
 
 router.post('/', validateAction, (req, res) => {
-
+  db.insert(req.body)
+    .then(dbRes => {
+      console.log(dbRes);
+      res.status(201).json({ accepted: req.body });
+    })
+    .catch(err => {
+      console.log(err);
+      this.res.status(500).json({ message: "internal error" });
+    });
 });
 
 router.get('/:id', (req, res) => {
@@ -20,7 +28,29 @@ router.delete('/:id', (req, res) => {
 });
 
 function validateAction(req, res, next) {
-  next();
+  const action = req.body;
+  if (!action) {
+    res.status(400).json({ message: "missing action data" });
+  } else if (!action.description) {
+    res.status(400).json({ message: "missing required description field"});
+  } else if (action.description > 128) {
+    res.status(400).json({ message: "description is too long"});
+  } else if (!action.notes) {
+    res.status(400).json({ message: "missing required notes field"});
+  } else {
+    db.get()
+      .then(gotten => {
+        if (gotten) {
+          next();
+        } else {
+          res.status(400).json({ message: "invalid project_id"});
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.res.status(500).json({ message: "internal error" });
+      });
+  }
 };
 
 module.exports = router;
