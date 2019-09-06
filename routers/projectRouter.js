092d94d1e69f871efdffddcd822e956d862c1abb
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-06 10:10:08 -0700

    projectRouter scaffolded

---
+++ routers/projectRouter.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const db = require('../data/helpers/projectModel');
+
+const router = express.Router();
+
+router.post('/', validateProject, (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+router.get('/:id/actions', (req, res) => {
+
+});
+
+
+function validateProject(req, res, next) {
+  const proj = req.body;
+  if (!proj) {
+    res.status(400).json({ message: "missing project data" });
+  } else if (!proj.name) {
+    res.status(400).json({ message: "missing required name field"});
+  } else if (!proj.description) {
+    res.status(400).json({ message: "missing required description field"})
+  } else {
+    next();
+  }
+};
+
+module.exports = router;
